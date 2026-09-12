33fc3fceb35f4848db06421ca3045e5a97317d4e
Franciss Peralta <francissper@gmail.com>
2026-09-11 23:57:30 -0500

    chore: scaffold SwiftUI app targeting iOS 17 with a clean ignore file

---
+++ FrameGate/FrameGateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameGateApp.swift
+//  FrameGate
+//
+//  Created by Franciss Peralta on 11/09/26.
+//
+
+import SwiftUI
+
+@main
+struct FrameGateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
